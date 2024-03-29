38b2ec102582ea33da73ce79137a2dfd81511266
‘guozhuang333’ <355948313@qq.com>
2024-03-29 13:48:15 +0800

    保存

--- internal/executor/contracts/select.go
+++ internal/executor/contracts/select.go
@@ -1,7 +1,6 @@
 package contracts
 
 import (
-	"fmt"
 	"github.com/meshplus/bitxhub-core/boltvm"
 )
 
@@ -15,17 +14,17 @@ func (t *SelectHost) Select(data []byte, addr string) *boltvm.Response {
 		temp := make(map[string][]byte)
 		t.Selects = temp
 	}
-	fmt.Println("存储时收到的加密内容", data)
-	fmt.Println("存储时收到的地址", addr)
+	//fmt.Println("存储时收到的加密内容", data)
+	//fmt.Println("存储时收到的地址", addr)
 	t.Selects[addr] = data
-	fmt.Println("存储后已经收到的条数", len(t.Selects))
-	fmt.Println("存储后的结果", t.Selects)
+	//fmt.Println("存储后已经收到的条数", len(t.Selects))
+	//fmt.Println("存储后的结果", t.Selects)
 	return boltvm.Success([]byte("选择成功"))
 }
 
 func (t *SelectHost) Verify(addr string) *boltvm.Response {
 	bytes, _ := t.Selects[addr]
-	fmt.Println("合约里收到的请求地址", addr)
-	fmt.Println("合约里面的输出加密内容", bytes)
+	//fmt.Println("合约里收到的请求地址", addr)
+	//fmt.Println("合约里面的输出加密内容", bytes)
 	return boltvm.Success(bytes)
 }
